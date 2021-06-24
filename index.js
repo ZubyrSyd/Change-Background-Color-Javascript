cfc13d4ca0e79d85289dea6aa40f00ef544f1354
ZubyrSyd <zubyr15@gmail.com>
2021-06-24 12:46:50 -0700

    Added the functionality so the background color changes when button is clicked.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Day 1');
+var color = ['yellow', 'black', 'blue', 'green'];
+var i = 0;
+document.querySelector("button").addEventListener("click",
+function(){
+    i = i < color.length - 1 ? ++i : 0; 
+    document.querySelector("body").style.background = color[i]
+    })
+
